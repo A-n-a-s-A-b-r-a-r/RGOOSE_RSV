b7f5c5b0d4cf43fc34568ecf520ca10219d3f30f
Anas <anasabrar4228@gmail.com>
2024-09-02 01:07:37 +0530

    added commands for windows support

--- run_recv.sh
+++ run_recv.sh
@@ -1,1 +1,4 @@
 sudo ./build/ied_recv sample.sed enp0s3 S2_IED0
+
+#for windows
+# build\ied_recv.exe sample.sed Ethernet S2_IED0
--- run_send.sh
+++ run_send.sh
@@ -1,1 +1,4 @@
 sudo ./build/ied_send sample.sed enp0s3 S1_IED22
+
+# for windows
+#build\ied_send.exe sample.sed Ethernet S1_IED22
