fd1b03903a708b52904e6c5f26a259b0b9608090
Hammad Javed <hammadxjaved@gmail.com>
2025-02-15 19:30:10 +0530

    added hmac signature

--- compression_encryption.py
+++ compression_encryption.py
@@ -6,6 +6,9 @@ NONCE_SIZE = 12  # Nonce size for AES-GCM in bytes
 TAG_SIZE = 16  # Tag size for AES-GCM in bytes
 AES_KEY_SIZE = 32  # AES-256 key size in bytes
 
+from cryptography.hazmat.primitives.hmac import HMAC
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
 
 def compress_data(data: bytes) -> bytes:
     return zlib.compress(data)
@@ -31,3 +34,8 @@ def decrypt_aes_gcm(ciphertext_with_nonce_and_tag: bytes) -> bytes:
     decryptor = cipher.decryptor()
     plaintext = decryptor.update(ciphertext) + decryptor.finalize()
     return plaintext
+
+def generate_hmac_cryptography(key, message):
+    h = HMAC(key, hashes.SHA256(), backend=default_backend())
+    h.update(bytes(message))  # Convert list of integers to bytes
+    return h.finalize()  # Return raw bytes (not hex)
